093c45d32e779a77349c70145b447b986fc4278a
DELL <DELL@DESKTOP-KLJI07S>
2023-04-03 14:34:52 +0700

    data mhs1

--- ActivityStucture_3 170/ActivityStucture_3 170.cpp
+++ ActivityStucture_3 170/ActivityStucture_3 170.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+struct Mahasiswa {
+	string nim;
+	string nama; 
+	string alamat;
+	int umur;
+};
+
+int main()
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.nim = "20210120011";
+	mhs1.nama = "Tasya";
+	mhs1.alamat = "Semarang";
+	mhs1.umur = 20;
+}
 
