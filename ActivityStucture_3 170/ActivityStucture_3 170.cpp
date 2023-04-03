14fe2269a8c6a46817a676f1a94cd6b614d6b6bb
DELL <DELL@DESKTOP-KLJI07S>
2023-04-03 14:43:27 +0700

    mhs 1

--- ActivityStucture_3 170/ActivityStucture_3 170.cpp
+++ ActivityStucture_3 170/ActivityStucture_3 170.cpp
@@ -17,5 +17,26 @@ int main()
 	mhs1.nama = "Tasya";
 	mhs1.alamat = "Semarang";
 	mhs1.umur = 20;
+
+	cout << "Masukkan nim: ";
+	cin >> mhs2.nim;
+	cout << "Masukkan nama: ";
+	cin >> mhs2.nama;
+	cout << "Masukkan alamat: ";
+	cin >> mhs2.alamat; 
+	cout << "Masukkan umur: ";
+	cin >> mhs2.umur; 
+
+	cout << "\nNim: " << mhs1.nim;
+	cout << "\nnama: " << mhs1.nama;
+	cout << "\nalamat: " << mhs1.alamat;
+	cout << "\nUmur: " << mhs1.umur;
+
+
+
+
 }
 
+
+
+ 
